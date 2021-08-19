b8795645df1e5929f304c04aa1eede7dbce81d64
imoni-chris <chris.imoni@africaprudential.com>
2021-08-19 11:30:36 +0100

    initial commit

---
+++ src/main/java/com/imonichris/myjavalearning/MyJavaLearningApplication.java
@@ -0,0 +1,13 @@
+package com.imonichris.myjavalearning;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyJavaLearningApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyJavaLearningApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/imonichris/myjavalearning/MyJavaLearningApplicationTests.java
@@ -0,0 +1,13 @@
+package com.imonichris.myjavalearning;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyJavaLearningApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
